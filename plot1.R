3169c29e517252de677eb4b821b3e174bf0c2d32
Katerina Touriki <k.touriki@gmail.com>
2014-08-06 19:03:02 +0300

    first commit

---
+++ plot1.R
@@ -0,0 +1,13 @@
+library("sqldf")
+
+sqldf("attach sample_db as new")
+
+read.csv.sql(file="~/R programming/sample_data.csv", sql = "create table df as select * from file ", dbname = "sample_db", header = T, row.names = F, sep = ";")
+df<-sqldf("select * from new where Date='1/2/2007' or Date='2/2/2007' ", dbname = "new_db")
+datetime<-paste(as.character(df$Date), as.character(df$Time))
+datetime<-strptime(datetime, "%d/ %m/ %Y %H: %M: %S")
+df$datetime<-datetime
+
+png("plot1.png", width=480, height=480)
+hist(df$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,21 @@
+
+library("sqldf")
+
+sqldf("attach sample_db as new")
+
+read.csv.sql(file="~/R programming/sample_data.csv", sql = "create table df as select * from file ", dbname = "sample_db", header = T, row.names = F, sep = ";")
+df<-sqldf("select * from new where Date='1/2/2007' or Date='2/2/2007' ", dbname = "new_db")
+datetime<-paste(as.character(df$Date), as.character(df$Time))
+datetime<-strptime(datetime, "%d/ %m/ %Y %H: %M: %S")
+df$datetime<-datetime
+
+png("plot3.png", width=480, height=480)
+
+plot(df$datetime, df$Sub_metering_2, type='l', col="red", ylim=range(c(df$Sub_metering_1,df$Sub_metering_2,df$Sub_metering_3)), ylab="Energy Sub metering", xlab="")
+par(new=TRUE)
+plot(df$datetime, df$Sub_metering_3, type='l', col="blue", ylim=range(c(df$Sub_metering_1,df$Sub_metering_2,df$Sub_metering_3)), ylab="Energy Sub metering", xlab="")
+par(new=TRUE)
+plot(df$datetime, df$Sub_metering_1, type='l', col="black", ylim=range(c(df$Sub_metering_1,df$Sub_metering_2,df$Sub_metering_3)), ylab="Energy Sub metering", xlab="")
+
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+dev.off()
